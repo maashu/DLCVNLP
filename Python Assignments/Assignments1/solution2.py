a717d2e5e37d6a1af8b1155a87dbd099faa5840f
maashu <anish.mahajan03@gmail.com>
2020-10-24 11:56:48 +0530

    Assignments 1 Solutions

---
+++ Python Assignments/Assignments1/solution2.py
@@ -0,0 +1,8 @@
+# 2 PROBLEM STATEMENT
+# Program to get user first and last name and print them in reverse order
+
+first_name = input('Enter your First Name:')
+last_name  = input('Enter your Last Name:')
+
+print(f'First and Last name in reverse order : {first_name[::-1]} {last_name[::-1]}')
+
---
+++ Python Assignments/Assignments1/solution3.py
@@ -0,0 +1,7 @@
+# 3 PROBLEM STATEMENT
+# Program to find diameter of sphere diameter = 12 cm
+
+diameter_in_cm = 12
+PI = 3.14
+volume_of_sphere = 4*PI*(diameter_in_cm)**3
+print(f'Volume of sphere: {volume_of_sphere}')
