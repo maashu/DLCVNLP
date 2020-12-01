aa79a14f4352fe320abcc79e797aeddb18b67265
maashu <anish.mahajan03@gmail.com>
2020-12-01 14:55:18 +0530

    Solution2.py

---
+++ Python Assignments/Assignment2/Solution2.py
@@ -0,0 +1,4 @@
+#2. Write a Python program to reverse a word after accepting the input from the user.
+
+word = input("Enter the string to be reversed: ")
+print(f' Reverse of the String {word} :{word[::-1]}')
